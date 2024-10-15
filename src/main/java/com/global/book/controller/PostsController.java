74f9bb5d9cb670056a84fc088f53bacb3300e74f
alhasnaa <alhasnaaelsayed1399@gmail.com>
2024-10-15 14:09:53 +0200

    integration with posts app

--- src/main/java/com/global/book/controller/PostsController.java
+++ src/main/java/com/global/book/controller/PostsController.java
@@ -3,17 +3,46 @@ package com.global.book.controller;
 import com.global.book.dto.PostDto;
 import com.global.book.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @Controller
-@RestController("/posts")
+@RequestMapping("/post")
 public class PostsController {
 
     @Autowired
     private PostService postService;
 
+   @GetMapping("/{id}")
+   public ResponseEntity<?> getPost(@PathVariable Long id){
 
-  //  public PostDto getPost
+       return   ResponseEntity.ok(postService.getPost(id));
+   }
+
+    @GetMapping
+    public ResponseEntity<?> getAllPost(){
+
+        return   ResponseEntity.ok(postService.getAllPost());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addPost(@RequestBody PostDto postDto){
+
+        return   ResponseEntity.ok(postService.addPost(postDto));
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updatePost(@RequestBody PostDto postDto){
+        postService.updatePost(postDto);
+        return ResponseEntity.ok(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletePost(@PathVariable Long id){
+        postService.deletePost(id);
+        return ResponseEntity.ok(null);
+    }
 
 }
--- src/main/java/com/global/book/service/PostService.java
+++ src/main/java/com/global/book/service/PostService.java
@@ -1,15 +1,19 @@
 package com.global.book.service;
 
 import com.global.book.dto.PostDto;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Service
 public class PostService {
 
 
-private static final String BASE_POST_URL="https://jsonplaceholder.typicode.com/posts";
+private static  String BASE_POST_URL="https://jsonplaceholder.typicode.com/posts";
 
     public PostDto getPost(Long id){
         RestTemplate restTemplate= new RestTemplate();
@@ -17,5 +21,34 @@ private static final String BASE_POST_URL="https://jsonplaceholder.typicode.com/
     return   response.getBody();
     }
 
+    public List<PostDto> getAllPost(){
+        RestTemplate restTemplate= new RestTemplate();
+        ResponseEntity<List> response= restTemplate.getForEntity(BASE_POST_URL ,List.class);
+        return   response.getBody();
+    }
+
+    public PostDto addPost( PostDto postDto){
+        RestTemplate restTemplate= new RestTemplate();
+        HttpHeaders headers=new HttpHeaders();
+        headers.add("accept","application/json");
+        headers.add("accept-language","en");
+
+        HttpEntity<PostDto> request=new HttpEntity<>(postDto,headers);
+        ResponseEntity<PostDto> response= restTemplate.postForEntity(BASE_POST_URL,request,PostDto.class);
+        return   response.getBody();
+    }
+
+    public void updatePost( PostDto postDto){
+        RestTemplate restTemplate= new RestTemplate();
+       HttpEntity<PostDto> request=new HttpEntity<>(postDto);
+         restTemplate.put(BASE_POST_URL,request);
+
+    }
+
+    public void deletePost( Long id){
+        RestTemplate restTemplate= new RestTemplate();
+        restTemplate.delete(BASE_POST_URL);
+
+    }
 
 }
